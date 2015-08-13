9bd166fab115d5ef079e4a850343ff5f79c8d8fa
panjan <jan.zdrahal@avg.com>
2015-08-13 16:22:11 +0200

    attachments route

--- lib/travis/guest-api/app/endpoints/logs.rb
+++ lib/travis/guest-api/app/endpoints/logs.rb
@@ -4,14 +4,12 @@ class Travis::GuestApi::App::Endpoints
   class Logs < Travis::GuestApi::App::Base
 
     before do
-      @job_id = env['job_id']
       @reporter = env['reporter']
     end
 
     post '/logs' do
-      payload = params.slice('job_id', 'message')
-      halt 422, { error: 'Key message and number must be passed'}.to_json unless payload['message']
-      @reporter.send_log(@job_id, payload["message"])
+      halt 422, { error: 'Key message and number must be passed'}.to_json unless params['message']
+      @reporter.send_log(@job_id, params["message"])
       { success: true }.to_json
     end
 
--- spec/integration/endpoints/logs_spec.rb
+++ spec/integration/endpoints/logs_spec.rb
@@ -8,19 +8,20 @@ module Travis::GuestApi
     include Rack::Test::Methods
 
     def app
-      Travis::GuestApi::App.new(1, reporter, &callback)
+      Travis::GuestApi::App.new(job_id, reporter, &callback)
     end
 
+    let(:job_id) { nil }
     let(:reporter) { double(:reporter) }
     let(:callback) { ->(x) { } }
 
     describe 'POST /logs' do
-      let(:post_data1) { { job_id: 1, message: 'my message1' } }
-      let(:post_data2) { { job_id: 1, message: 'my message2' } }
+      let(:post_data1) { { job_id: 123, message: 'my message1' } }
+      let(:post_data2) { { job_id: 456, message: 'my message2' } }
 
-      it 'sends data to pusher' do
-        expect(reporter).to receive(:send_log).with(1, post_data1[:message])
-        expect(reporter).to receive(:send_log).with(1, post_data2[:message])
+      it 'sends data to reporter' do
+        expect(reporter).to receive(:send_log).with(post_data1[:job_id], post_data1[:message])
+        expect(reporter).to receive(:send_log).with(post_data2[:job_id], post_data2[:message])
 
         response = post '/api/v2/logs', post_data1.to_json, "CONTENT_TYPE" => "application/json"
         expect(response.status).to eq(200)
@@ -34,14 +35,14 @@ module Travis::GuestApi
         expect(response.status).to eq(422)
       end
 
-      it 'responds with 422 on job_id mismatch' do
-        response = post '/api/v2/logs', { job_id: 2 }.to_json, "CONTENT_TYPE" => "application/json"
-        expect(response.status).to eq(422)
+      context 'server run with job_id' do
+        let(:job_id) { 42 }
+        it 'sends data to pusher' do
+          payload_without_job_id = { message: 'my message1' }
+          expect(reporter).to receive(:send_log).with(42, post_data1[:message])
+          post '/api/v2/logs', payload_without_job_id.to_json, "CONTENT_TYPE" => "application/json"
+        end
       end
-    end
-
-    describe 'correctly rewrites form v1' do
-      #post '/api/v1/messages/logs/message',
     end
 
     describe 'POST /jobs/:job_id/logs' do
